8357bd0b3eee46bb248f5bcdcf8876b7ed68c063
Catalin.Iancu <IancuCatalinCristian@protonmail.com>
2022-01-16 11:13:03 +0200

    Forma finala

--- main.cpp
+++ main.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
+
 using namespace std;
 
-/* In vectorul s imi voi stoca submultimile generate.
- * In variabila nrsol imi stochez numarul de solutii corecte generate.
+/*
+ -> In vectorul s imi voi stoca submultimile generate.
+ -> In variabila nrsol imi stochez numarul de solutii corecte generate.
 */
 int s[101], n, nrsol;
 
-/* Aici ma asigur ca :
+/*
+ Aici ma asigur ca :
  -> Nu exista o diferenta mai mare de 2 intre 2 termeni consecutivi.
  -> Ultimul termen, adica s[m] va fi de fiecare data egal cu n.
 */
-bool solp(int k, int m)
+bool SolutiePartiala(int k, int m)
 {
     if (s[k] - s[k - 1] > 2)
     {
@@ -23,32 +26,54 @@ bool solp(int k, int m)
     return true;
 }
 
-/* Aici imi generez aranjamentele. Functia va fi recursiva si va primi 2 parametrii :
- * int k care reprezinta indicele solutiei anterioare
- * int m care reprezinta dimensiunea submultimii pe care vreau sa o generez
+void Afisare(int k)
+{
+    for (int i = 1; i <= k; i++)
+    {
+        cout << s[i] << ", ";
+    }
+    cout << "\b\b " << endl;
+}
+
+/*
+ Aici imi generez aranjamentele. Functia va fi recursiva si va primi 2 parametrii :
+ -> int k care reprezinta indicele solutiei anterioare
+ -> int m care reprezinta dimensiunea submultimii pe care vreau sa o generez
 */
-void bkt(int k, int m)
+void Backtracking(int k, int m)
 {
     int v;
-    for (v = s[k - 1] + 1; v <= n; v++) // Din moment ce vreau ca elementele submultimii sa fie strict crescatoare, voi porni de la v = s[k-1] + 1
+/*
+ -> Voi porni de la v = s[k-1] pentru a ma asigura ca elementele generate sunt crescatoare.
+ -> De asemenea imi voi reduce din timpul de executie pornind generarea noului element de la valoarea elementului anterior + 1
+*/
+    for (v = s[k - 1] + 1; v <= n; v++)
     {
         {
             s[k] = v;
-            if (solp(k, m)) // Aici are loc validarea solutiei partiale
+            if (SolutiePartiala(k, m)) // Validez daca solutia partiala este valida
             {
-                if (k == m) // Aici imi dau seama daca solutia este completa sau partiala. Daca este partiala, voi chema din nou functia bkt(k,m) cu un k incrementat cu 1
+                if (k == m) // Validez daca solutia este finala
                 {
-                    nrsol++;
-                    for (int i = 1; i <= k; i++)
-                        cout << s[i] << ", ";
-                    cout << "\b\b " << endl;
+                    nrsol++; // Incrementez counterul de solutii (cerinta problemei)
+                    Afisare(k); // Afisez solutia finala
                 } else
                 {
-                    bkt(k + 1, m);
+                    Backtracking(k + 1, m);
                 }
             }
         }
     }
+}
+
+//Aici apelez functia Backtracking care va genera toate submultimile de dimensiune m
+void Rezolvare(int n)
+{
+    for (int m = 1; m <= n; m++)
+    {
+        Backtracking(1, m);
+    }
+    cout << endl;
 }
 
 int main()
@@ -57,15 +82,10 @@ int main()
     cin >> n;
     cout << endl;
 
-    cout << "Problema va afisa treptele pe care le va urca cetateanul: ";
+    cout << "Cetateanul turmentat are urmatoarele optiuni: ";
     cout << endl;
 
-    //Aici apelez functia bkt care va genera toate submultimile de dimensiune m (eliminand submultimile care nu indeplinesc cele 3 conditii din enunt)
-    for (int m = 1; m <= n; m++)
-    {
-        bkt(1, m);
-    }
-    cout << endl;
+    Rezolvare(n);
 
     cout << "Numar solutii: ";
     cout << nrsol;
